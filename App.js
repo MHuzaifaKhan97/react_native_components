ce59309a0a1e3fc1ae284bf93001593ae718693d
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-02-07 14:40:24 +0500

    Learn React Native Functionalities

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import GeolocationExample from './src/components/GeolocationExample';
+const App = () => {
+  return(
+    <GeolocationExample />
+  )
+}
+export default App;
