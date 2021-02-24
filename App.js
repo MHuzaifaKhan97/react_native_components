231e2d70e6b0ef010fc8ba4048e4543e574e5fae
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-02-25 00:00:16 +0500

    Animated Login Init

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 // import GeolocationExample from './src/geolocation/GeolocationExample';
 import MyCustomDrawer from './src/drawernavigation/myCustomDrawer';
+import AnimatedLogin from './src/animatedLogin/AnimatedLogin';
+
 const App = () => {
   return(
     // <GeolocationExample />
-    <MyCustomDrawer />
+    // <MyCustomDrawer />
+    <AnimatedLogin />
   )
 }
 export default App;
---
+++ src/animatedLogin/AnimatedLogin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const AnimatedLogin = () => {
+    return (
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Animated Login</Text>
+        </View>
+    )
+}
+export default AnimatedLogin;
+
+const styles = StyleSheet.create({
+
+})
